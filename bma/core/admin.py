e7e463e913d8f762739e72a09b89591672d73d60
Singh-jot85 <gurjot.singh@thesummitgrp.com>
2025-10-20 00:08:36 +0530

    test rulesets

--- bma/core/admin.py
+++ bma/core/admin.py
@@ -257,11 +257,6 @@ class BaseUserModelAdmin(admin.ModelAdmin):
         its superclass implementation but is customized because the User model
         has a slightly different workflow.
         """
-        # We should allow further modification of the user just added i.e. the
-        # 'Save' button should behave like the 'Save and continue editing'
-        # button except in two scenarios:
-        # * The user has pressed the 'Save and add another' button
-        # * We are adding a user in a popup
         if "_addanother" not in request.POST and IS_POPUP_VAR not in request.POST:
             request.POST = request.POST.copy()
             request.POST["_continue"] = 1
